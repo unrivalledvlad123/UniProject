76fcc196e0abe749238b7b6f30d5f6306308f6c4
Vlado <vladunvlad@abv.bg>
2017-02-27 18:01:11 +0200

    DB
    DB

--- DB3/Company.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Company]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Name] VARCHAR(100) NOT NULL, 
-    [Address] VARCHAR(500) NOT NULL, 
-    [Mol_id] INT NOT NULL, 
-    [Phone] VARCHAR(50) NULL, 
-    [Email] VARCHAR(50) NULL
-)
