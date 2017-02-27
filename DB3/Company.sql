f58fb2bbdd211093ddc103b16ea640c6c603adb9
Vlado <vladunvlad@abv.bg>
2017-02-27 17:23:17 +0200

    latest
    atest

---
+++ DB3/Company.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Company]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] VARCHAR(100) NOT NULL, 
+    [Address] VARCHAR(500) NOT NULL, 
+    [Mol_id] INT NOT NULL, 
+    [Phone] VARCHAR(50) NULL, 
+    [Email] VARCHAR(50) NULL
+)
